9feb71b194f35f458d6ea12e34ada2b249a7373e
mitulgadhiya <88765850+mitullgadhiya@users.noreply.github.com>
2022-07-05 14:58:35 +0530

    smart contract structure

---
+++ 1_SimpleSmartContract/SimpleSmartContract.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+// @dev structure of the smart contract
+contract SimpleSmartContract {
+    // All the logic part stays inside the contract. 
+}
